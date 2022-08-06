a4bfcf1635862daf7b53716548e81c7d2849ece0
andy19910102 <andy19910102@gmail.com>
2022-08-06 20:07:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,15 +2,16 @@ const { YOUTUBE_API_KEY } = require('./config.json');
 const createMarion = require("./index.js")
 
 // Create the instance of Marion
-const marion = createMarion(YOUTUBE_API_KEY)
+const marion = createMarion(YOUTUBE_API_KEY);
+
 // Get some funny cat videos
-// marion.getFunnyCatVideos()
-//     .then(res => {
-//         console.log(res);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
+marion.getFunnyCatVideos(3)
+    .then(videoList => {
+        console.log("videoList", videoList);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 
 // Export CSV
-marion.exportFunnyCatVideosCSV()
+marion.exportFunnyCatVideosCSV("funny_cat_videos.csv", 3);
